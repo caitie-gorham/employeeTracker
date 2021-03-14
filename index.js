79431f0e25db5f6ead8c37c0f23167fd2d431454
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-03-13 19:17:11 -0600

    split function, tested for functionality

--- index.js
+++ index.js
@@ -8,83 +8,89 @@ connection.connect((err) => {
 
     // console log connection thread ID
     console.log(`Connection established via thread #: ${connection.threadId}.`);
-    
+
     // kick off function that has inquirer prompt and the function start for each choice
     start();
 });
 
 // start function with inquirer prompt and switch/case for each choice
-const start = () => {
+function start() {
     inquirer.prompt([
         {
             type: 'list',
             message: 'What would you like to do?',
             name: 'choices',
             choices: [
-                'View all employees', 
-                'View all employees by department', 
-                'View all employees by manager', 
+                'View all employees',
+                'View all employees by department',
+                'View all employees by manager',
                 'Add employee',
                 'Remove employee',
-                'Update employee role', 
+                'Update employee role',
                 'Update employee manager',
-                'View all roles', 
-                'Add role', 
+                'View all roles',
+                'Add role',
                 'View all departments',
-                'Add department', 
-                'Quit']
+                'Add department',
+                'Quit'
+            ]
         }
-    ]).then((response) => {
-        switch(response.choices) {
-            case 'View all employees':
-                viewEmployees()
-                break;
-
-            case 'View all employees by department':
-                viewEmployeesDep()
-                break;
-
-            case 'View all employees by manager':
-                viewEmployeesMan()
-                break;
-            
-            case 'Add employee':
-                addEmployee()
-                break;
-
-            case 'Remove employee':
-                removeEmployee()
-                break;
-
-            case 'Update employee role':
-                updateEmployeeRole()
-                break;
-
-            case 'Update employee manager':
-                updateEmployeeManager()
-                break;
-
-            case 'View all roles':
-                viewRoles()
-                break;
-
-            case 'Add role':
-                addRole()
-                break;
-
-            case 'View all departments':
-                viewDepartment()
-                break;
-
-            case 'Add department': 
-                addDepartment()
-                break;
-            
-            case 'Quit':
-                connection.end()
-                break;
-
+    ]).then(function ({ choices }) {
+        if (choices != "Quit") {
+            processChoice(choices, function () {
+                start();
+            })
+        } else {
+            connection.end()
         }
+        console.log(choices);
+    });
+};
 
-    }).catch(err => {console.error(err)});
+function switchCase(choices, spec) {
+    switch (choices) {
+        case 'View all employees':
+            viewEmployees(spec)
+            break;
+
+        case 'View all employees by department':
+            viewEmployeesDep(spec)
+            break;
+
+        case 'View all employees by manager':
+            viewEmployeesMan(spec)
+            break;
+
+        case 'Add employee':
+            addEmployee(spec)
+            break;
+
+        case 'Remove employee':
+            removeEmployee(spec)
+            break;
+
+        case 'Update employee role':
+            updateEmployeeRole(spec)
+            break;
+
+        case 'Update employee manager':
+            updateEmployeeManager(spec)
+            break;
+
+        case 'View all roles':
+            viewRoles(spec)
+            break;
+
+        case 'Add role':
+            addRole(spec)
+            break;
+
+        case 'View all departments':
+            viewDepartment(spec)
+            break;
+
+        case 'Add department':
+            addDepartment(spec)
+            break;
+    }
 }
